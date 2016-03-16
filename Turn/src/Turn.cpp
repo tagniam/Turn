b66ad6ba246a9cb98352bd0158db2f0f4bccf485
tagniam <tagniam@users.noreply.github.com>
2016-03-16 01:46:05 -0400

    Revert "Moved comments into right places"
    This reverts commit a3f3210f73e017b304eebcc9614c03bd033a80bc.

--- Turn/src/Turn.cpp
+++ Turn/src/Turn.cpp
@@ -8,6 +8,7 @@
 // - add advanced AI (when taking a risk attack and miss, heal)
 // - store and sell (buy with coins, sell arrows/bombs/etc for money or experience)
 // - use items in between matches (whetstone, potion)
+// - save data in file
 
 /// Continue commenting code at Player.GenericAttack().
 
