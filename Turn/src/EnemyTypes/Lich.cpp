482c714c3857913f57fe415f66be5ada29e45bf2
Ryan Dern <ryanmdern@gmail.com>
2017-10-17 19:31:21 -0400

    Added new Lich enemy class

---
+++ Turn/src/EnemyTypes/Lich.cpp
@@ -0,0 +1,30 @@
+#include "..\..\include\EnemyTypes\Lich.h"
+
+Lich::Lich() {
+	name = "Lich";
+	ExperienceAmount = 100;
+	CoinsDrop = 50 + rand() % 100;
+}
+
+int Lich::ReturnDamage() {
+	return 5 + rand() % 20;
+}
+
+int Lich::ReturnRiskAttackDamage() {
+	int selector = rand() % 6;
+	switch (selector) {
+	case 0: case 1: case 2: case 3:
+		return 2;
+		break;
+	case 4: case 5:
+		return 30;
+		break;
+	default:
+		return 0;
+		break;
+	}
+}
+
+int Lich::ReturnHealAmount() {
+	return 5 + rand() % 25;
+}
