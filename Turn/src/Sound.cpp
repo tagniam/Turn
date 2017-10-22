c6f45d6cd560df989075bd39dda89076116757e4
kevinmmurphy <kevinmmurphy@gmail.com>
2017-10-21 23:46:37 -0400

    Source credits for sound files

--- Turn/src/Sound.cpp
+++ Turn/src/Sound.cpp
@@ -3,6 +3,12 @@
 #include <Windows.h>
 #include <MMSystem.h>
 
+
+// Sounds found online at:
+//  https://freesound.org/people/Ali_6868/sounds/384915/ arrow
+//  https://freesound.org/people/TreyHolton/sounds/238307/ heal
+//  https://freesound.org/people/Taira%20Komori/sounds/213696/ attack
+
 SoundMaker::SoundMaker(void):attackFileName("sounds/attack.wav"),
 							 attack2FileName("sounds/arrow.wav"),
 							 healFileName("sounds/heal.wav"){
