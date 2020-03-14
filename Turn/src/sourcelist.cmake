659713fdd2181556d92fdbda44f4e0c1f3c8ed53
John-Doe-The-Robot <59103888+John-Doe-The-Robot@users.noreply.github.com>
2020-03-14 01:49:46 +0100

    Update sourcelist.cmake

--- Turn/src/sourcelist.cmake
+++ Turn/src/sourcelist.cmake
@@ -33,6 +33,7 @@ set (SOURCES ${SOURCES}
     src/EnemyTypes/Werewolf.cpp
     src/EnemyTypes/Zabra.cpp
     src/EnemyTypes/Zombie.cpp
+    src/EnemyTypes/Demon.cpp
 
     src/PlayerTypes/Debugger.cpp
     src/PlayerTypes/Healer.cpp
