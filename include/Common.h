925ec3e88b428a7c3178d9af75a7764a4ad86c93
DJ73 <d.max1998@gmail.com>
2020-10-30 09:34:44 +0530

    Remove excess includes

--- include/Common.h
+++ include/Common.h
@@ -3,7 +3,6 @@
 
 #include <string>
 #include <vector>
-#include <numeric>
 #include <iostream>
 
 #include "../include/Console.h"
--- src/Common.cpp
+++ src/Common.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <vector>
 #include <chrono>
 #include <thread>
 #include <random>
